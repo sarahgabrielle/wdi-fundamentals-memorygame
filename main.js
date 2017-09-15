3af17e7834f9017f116f90033121f7377cc4e0d2
sarahalpay <sarah.g.alpay@gmail.com>
2017-09-15 11:24:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
+};
 
-cardsInPlay.push(cardOne);
+var flipCard = function(cardId) {
+console.log("User flipped " + cards[cardId])
+cardsInPlay.push(cards[cardId])
+};
 
-console.log("User flipped" + " " + cardsInPlay);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped" + " " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
- } 
- else {
- 	alert("Sorry, try again.");
- }
-
+flipCard(0);
+flipCard(2);
+checkForMatch();
