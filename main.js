5b5935dba2e0001edacaa08c13cf35c4f2346676
sarahalpay <sarah.g.alpay@gmail.com>
2017-09-16 21:58:14 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -24,22 +24,37 @@ cardImage: "images/king-of-diamonds.png"
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
 	} else {
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank)
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
+this.setAttribute('src', cards[cardId].cardImage);
+
+if(cardsInPlay.length === 2) {
+	checkForMatch();
+}
 };
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+var createBoard = function() {
+for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard();
+
 
 
